c046ebc380ef5effe1bcb748cf2500b021006b95
Vyacheslav <pronin.slavok@mail.ru>
2021-07-25 12:38:02 +0300

    Создание проекта

---
+++ Blogs/Blogs/StartViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Blogs
+//
+//  Created by Vyacheslav Pronin on 25.07.2021.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .systemGreen
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+
+}
+
