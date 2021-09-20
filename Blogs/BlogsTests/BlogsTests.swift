771060c72c1f5b82c43aac200ebd9a333e99fc97
Vyacheslav <pronin.slavok@mail.ru>
2021-09-20 22:40:30 +0300

    сделал тесты

--- Blogs/BlogsTests/BlogsTests.swift
+++ Blogs/BlogsTests/BlogsTests.swift
@@ -12,9 +12,46 @@ import XCTest
 
 class BlogsTests: XCTestCase {
     
+    var previewContainer: PreviewContainer!
     var loginContainer: LoginContainer!
-    
+    var signUpContainer: SignUpContainer!
+    var blogContainer: BlogContainer!
+    var userManager: UserManager!
+    var blog: Blog!
+
     override func setUp() {
+        super.setUp()
+        previewContainer = PreviewContainer.assemble(with: .init())
         loginContainer = LoginContainer.assemble(with: .init())
+        signUpContainer = SignUpContainer.assemble(with: .init())
+        blogContainer = BlogContainer.assemble(with: .init())
+        blog = Blog(user: defaultUser,
+                    title: "Title",
+                    dateCreate: .init(),
+                    finalPost: Post(date: .init(),
+                                    title: "Title",
+                                    text: "Text",
+                                    arrayTags: ["1", "2"]),
+                    arrayTags: ["1", "2"],
+                    arrayLikeUsers: Set(),
+                    arrayShareUsers: Set(),
+                    rating: 0,
+                    identifier: "")
+    }
+
+    override func tearDown() {
+        previewContainer = nil
+        loginContainer = nil
+        signUpContainer = nil
+        blog = nil
+        super.tearDown()
+    }
+
+    func test_is_valid_controller() throws {
+        XCTAssertNoThrow(previewContainer.viewController.view)
+        XCTAssertNoThrow(loginContainer.viewController.view)
+        XCTAssertNoThrow(signUpContainer.viewController.view)
+        XCTAssertNoThrow(blogContainer.input.setupBlog(blog: blog))
+        XCTAssertNoThrow(blogContainer.viewController.view)
     }
 }
