3ebdbab9945f1ddd7dd19dca118bf90aa505531c
Vyacheslav <pronin.slavok@mail.ru>
2021-09-20 16:44:35 +0300

    Обновил firebase

--- Blogs/BlogsTests/BlogsTests.swift
+++ Blogs/BlogsTests/BlogsTests.swift
@@ -2,32 +2,19 @@
 //  BlogsTests.swift
 //  BlogsTests
 //
-//  Created by Vyacheslav Pronin on 25.07.2021.
+//  Created by Vyacheslav Pronin on 20.09.2021.
 //
 
+
 import XCTest
+
 @testable import Blogs
 
 class BlogsTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    var loginContainer: LoginContainer!
+    
+    override func setUp() {
+        loginContainer = LoginContainer.assemble(with: .init())
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
---
+++ Blogs/Pods/Target Support Files/Pods-Blogs-BlogsTests/Pods-Blogs-BlogsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Blogs_BlogsTests : NSObject
+@end
+@implementation PodsDummy_Pods_Blogs_BlogsTests
+@end
